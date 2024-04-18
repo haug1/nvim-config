f3871856ade731da86a10f92a5f37d56dc8a4a32
Lars-Ragnar A. Haugen <lars.ragnar.haugen@origo.oslo.kommune.no>
2024-04-18 19:48:03 +0200

    fix: tmux-navigator disabled lazygit keymap

--- lua/haug1/init.lua
+++ lua/haug1/init.lua
@@ -14,7 +14,20 @@ require("lazy").setup({
   "nvim-lua/plenary.nvim",
   "tpope/vim-sleuth",
   { "numToStr/Comment.nvim", opts = {} },
-  "christoomey/vim-tmux-navigator",
+  {
+    "christoomey/vim-tmux-navigator",
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-q>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+    opts = function(_, __)
+      -- NOTE: Do not set default mappings
+      vim.g.tmux_navigator_no_mappings = 1
+    end,
+  },
   "stevearc/dressing.nvim",
   "mg979/vim-visual-multi",
   {
