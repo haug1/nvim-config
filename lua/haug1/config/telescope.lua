53f83ad65ada46118d8ec80ccb85acd6f5f7e0d0
Lars-Ragnar A. Haugen <lars.ragnar.haugen@origo.oslo.kommune.no>
2024-04-27 20:01:50 +0200

    feat(telescope): browse .config files

--- lua/haug1/config/telescope.lua
+++ lua/haug1/config/telescope.lua
@@ -6,7 +6,13 @@ local action_set = require("telescope.actions.set")
 local action_state = require("telescope.actions.state")
 local conf = require("telescope.config").values
 
+local config_dir = vim.fn.expand("$HOME/.config")
+
 local M = {}
+
+function M.config_files()
+  builtin.find_files({ cwd = config_dir })
+end
 
 function M.dirjump_file(dir)
   M.dirjump(dir, builtin.find_files)
