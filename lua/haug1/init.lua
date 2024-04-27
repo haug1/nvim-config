66e5487d618a8a438664c500f561fc95284df23a
Lars-Ragnar A. Haugen <lars.ragnar.haugen@origo.oslo.kommune.no>
2024-04-27 21:45:04 +0200

    refactor: move lazy-bootstrap to core

--- lua/haug1/init.lua
+++ lua/haug1/init.lua
@@ -1,7 +1,7 @@
 require("haug1.core.options")
 require("haug1.core.keymaps")
 require("haug1.core.autocmds")
-require("haug1.lazy")
+require("haug1.core.lazy")
 require("lazy").setup({
   { import = "haug1.plugins" },
   { import = "haug1.plugins.lspconfig" },
