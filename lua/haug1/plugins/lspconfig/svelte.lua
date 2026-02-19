4d22329ad7b70455c38190b8c4e45e5064760c89
haug1 <lars-ragnar.antell.haugen@dig.oslo.kommune.no>
2026-02-19 20:28:35 +0100

    stuff

---
+++ lua/haug1/plugins/lspconfig/svelte.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "svelte" })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      opts.servers = opts.servers or {}
+      opts.servers.svelte = {}
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        svelte = { "prettier" },
+      },
+    },
+  },
+}
