89b7e6a7b297103c5f92f5ceead069e20ef65519
haug1 <lars-ragnar.antell.haugen@dig.oslo.kommune.no>
2026-03-04 23:18:17 +0100

    even better stuffs

--- lua/haug1/plugins/lspconfig/lint.lua
+++ lua/haug1/plugins/lspconfig/lint.lua
@@ -7,6 +7,20 @@ return {
       local lint = require("lint")
       lint.linters_by_ft = opts.linters_by_ft
 
+      local ktlint_default_editorconfig = vim.fn.stdpath("config")
+        .. "/lua/haug1/config/ktlint/defaults.editorconfig"
+      if lint.linters.ktlint then
+        lint.linters.ktlint.args = function()
+          local filename = vim.api.nvim_buf_get_name(0)
+          return {
+            "--reporter=json",
+            "--editorconfig=" .. ktlint_default_editorconfig,
+            "--stdin",
+            "--stdin-path=" .. filename,
+          }
+        end
+      end
+
       local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
       vim.api.nvim_create_autocmd(
