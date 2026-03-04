e868bb884145da4aecadb9265f2299f8e6fddcb0
haug1 <lars-ragnar.antell.haugen@dig.oslo.kommune.no>
2026-03-04 22:58:14 +0100

    lsp attaches

--- lua/haug1/plugins/lspconfig/kotlin.lua
+++ lua/haug1/plugins/lspconfig/kotlin.lua
@@ -1,41 +1,98 @@
 return {
-  -- {
-  --   "WhoIsSethDaniel/mason-tool-installer.nvim",
-  --   opts = function(_, opts)
-  --     vim.list_extend(
-  --       opts.ensure_installed,
-  --       { "kotlin_language_server", "ktlint" }
-  --     )
-  --   end,
-  -- },
-  -- {
-  --   "nvim-treesitter/nvim-treesitter",
-  --   opts = function(_, opts)
-  --     vim.list_extend(opts.ensure_installed, { "kotlin" })
-  --   end,
-  -- },
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   opts = {
-  --     servers = {
-  --       kotlin_language_server = {},
-  --     },
-  --   },
-  -- },
-  -- {
-  --   "stevearc/conform.nvim",
-  --   opts = {
-  --     formatters_by_ft = {
-  --       ["kotlin"] = { "ktlint" },
-  --     },
-  --   },
-  -- },
-  -- {
-  --   "mfussenegger/nvim-lint",
-  --   opts = {
-  --     linters_by_ft = {
-  --       kotlin = { "ktlint" },
-  --     },
-  --   },
-  -- },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "kotlin_language_server" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "ktlint", "kotlin-debug-adapter" })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      opts.servers = opts.servers or {}
+
+      local java21_candidates = vim.fn.glob(
+        vim.fn.expand("$HOME/.sdkman/candidates/java/21*"),
+        false,
+        true
+      )
+      local java21_home = java21_candidates[1]
+
+      local server_opts = {}
+      if java21_home and java21_home ~= "" and vim.fn.isdirectory(java21_home) == 1 then
+        server_opts.cmd_env = {
+          JAVA_HOME = java21_home,
+          PATH = java21_home .. "/bin:" .. vim.env.PATH,
+        }
+      end
+
+      opts.servers.kotlin_language_server = vim.tbl_deep_extend(
+        "force",
+        opts.servers.kotlin_language_server or {},
+        server_opts
+      )
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        kotlin = { "ktlint" },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        kotlin = { "ktlint" },
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "Mgenuit/neotest-kotlin",
+    },
+    opts = {
+      adapters = {
+        ["neotest-kotlin"] = {},
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    config = function()
+      local dap = require("dap")
+
+      local mason_adapter = vim.fn.stdpath("data")
+        .. "/mason/bin/kotlin-debug-adapter"
+      local adapter_cmd = vim.fn.executable(mason_adapter) == 1 and mason_adapter
+        or "kotlin-debug-adapter"
+
+      dap.adapters.kotlin = {
+        type = "executable",
+        command = adapter_cmd,
+        args = {},
+      }
+
+      dap.configurations.kotlin = {
+        {
+          type = "kotlin",
+          request = "launch",
+          name = "Launch Kotlin (prompt for main class)",
+          projectRoot = "${workspaceFolder}",
+          mainClass = function()
+            return vim.fn.input("Main class: ")
+          end,
+        },
+      }
+    end,
+  },
 }
