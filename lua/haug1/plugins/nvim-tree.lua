539121d688eb72cc0022c0a524b5675f9c7dbf57
haug1 <lars-ragnar.antell.haugen@dig.oslo.kommune.no>
2026-06-24 18:41:12 +0200

    fix: disable netrw early so nvim-tree owns directories

--- lua/haug1/plugins/nvim-tree.lua
+++ lua/haug1/plugins/nvim-tree.lua
@@ -8,9 +8,7 @@ return {
 
       local nvimtree = require("nvim-tree")
 
-      -- recommended settings from nvim-tree documentation
-      vim.g.loaded_netrw = 1
-      vim.g.loaded_netrwPlugin = 1
+      -- netrw is disabled early in core/options.lua
 
       nvimtree.setup({
         view = {
