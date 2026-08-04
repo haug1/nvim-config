cd4ad10114414e845195474f0dd718f7929268ec
haug1 <lars-ragnar.antell.haugen@dig.oslo.kommune.no>
2026-08-04 13:14:15 +0200

    feat: svelte lsp

--- lua/haug1/plugins/lspconfig/svelte.lua
+++ lua/haug1/plugins/lspconfig/svelte.lua
@@ -6,6 +6,12 @@ return {
     end,
   },
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "svelte" })
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
       opts.servers = opts.servers or {}
